7754d063bbe549b55777673ad240be47109db69e
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-10-03 19:55:37 +0200

    add class to palindroms

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-console.log('Funguju!')
-
 const textPozpatku = (text) => {
 	return text.split('').reverse().join('')
 }
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+
+const items = document.querySelectorAll('li');
+items.forEach((li) => {
+	if (li.textContent && li.textContent.toLowerCase() === textPozpatku(li.textContent.toLowerCase())) {
+		li.classList.add('palindrom');
+	}
+})
